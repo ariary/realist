fa5483470a83e241ddf9a833418886ccfa19b0bc
ariary <ariary9.2@hotmail.fr>
2022-04-07 14:47:14 +0200

    Carthesian product without dynamic printing

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,10 @@ fn main() {
     
     let minlength = matches.value_of("min-len");
     match minlength {
-        None => println!("No idea what your favorite number is."),
+        None => minlen =4,  //default
         Some(s) => {
             match s.parse::<usize>() {
-                Ok(n) => minlen =n,
+                Ok(n) => minlen = n,
                 Err(_) =>{
                     eprintln!("--min-len ({}) is not a number", s);
                     process::exit(1);
@@ -51,9 +51,25 @@ fn main() {
     };
     println!("{}", my_scores);
 
-    for perm in numbers.iter().permutations(minlen).unique() {
+    for perm in numbers.iter().permutations(11).unique() {
         // let word = Word{characters:perm};
         // println!("{}",word)
         println!("{:?}", perm);
     }
+
+    // Create alphabet characters
+    // let alphabet = (b'A'..=b'z')           // Start as u8
+    // .map(|c| c as char)            // Convert all to chars
+    // .filter(|c| c.is_alphabetic()) // Filter only alphabetic chars
+    // .collect::<Vec<_>>();          // Collect as Vec<char>
+    // println!("{:?}", alphabet);
+
+    let characters = vec!["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let n =minlen;
+    println!("{:?}", minlen);
+    let combinations : Vec<_> = (2..minlen).fold(
+        characters.iter().cartesian_product(characters.iter()).map(|(&a, &b)| a.to_owned() + b).collect(),
+        |acc, _| acc.into_iter().cartesian_product(characters.iter()).map(|(a, b)| a.to_owned() + b).collect()
+    );
+    println!("{:?}", combinations);
 }
